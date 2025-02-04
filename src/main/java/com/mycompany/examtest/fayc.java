31fa1b4d6968d1a0adf9914716e5a92df4a61c73
Mohammed Fay�al Bouhadiba <2417045@grasset.qc.ca>
2025-02-04 15:46:27 -0500

    classee fay

---
+++ src/main/java/com/mycompany/examtest/fayc.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.examtest;
+
+/**
+ *
+ * @author 2417045
+ */
+public class fayc {
+     private int id;
+    private String salle;
+    private String date;
+}
